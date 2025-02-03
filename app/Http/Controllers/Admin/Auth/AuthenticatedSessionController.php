25afaa8d3e8aff5ac1e4f4ad5fc85203755ae491
thikko <aosaku706@icloud.com>
2025-02-04 00:27:20 +0900

    会員トップページ作成

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
        $request->session()->regenerate();
 
-       return redirect()->intended(RouteServiceProvider::ADMIN_HOME);
+       return redirect()->intended(RouteServiceProvider::ADMIN_HOME)->with('flash_message', 'ログインしました。');
    }
 
    /**
@@ -43,6 +43,6 @@ class AuthenticatedSessionController extends Controller
 
        $request->session()->regenerateToken();
 
-       return redirect('/');
+       return redirect('/')->with('flash_message', 'ログアウトしました。');
    }
 }
