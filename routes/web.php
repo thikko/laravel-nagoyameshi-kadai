c44f2e1a4f12dd89ac84aea4c441b62af9132850
thikko <aosaku706@icloud.com>
2025-02-10 16:47:43 +0900

    会員側の会員管理機能

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use App\Http\Controllers\Admin\RestaurantController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CompanyController;
+use App\Http\Controllers\Admin\TermController;
+
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +24,10 @@ use App\Http\Controllers\HomeController;
 
 Route::group(['middleware' => 'guest:admin'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    Route::group(['middleware' => ['auth', 'verified']], function () {
+        Route::resource('user',UserController::class)->only(['index', 'edit', 'update']);
+    });  
 });
 
 
